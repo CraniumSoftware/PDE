1059cf3bc425bdb603b43df5d07a3a4be0187801
Semi Essessi <semi@cranium-software.co.uk>
2015-02-28 14:29:46 +0000

    crappy profile timer using weakly implemented C++ 11 stuff

---
+++ Time/ProfileTimer.cpp
@@ -0,0 +1,21 @@
+#include "ProfileTimer.h"
+
+// SE - for now... just do this dumbly and trust that we have very modern C++
+// ... there are platform implementations in Cerebro that I can nick if this
+// becomes a genuine problem
+
+#include <chrono>
+
+double GetProfileTimer()
+{
+    // SE - NOTE: would it have hurt so much to not nest these things to the
+    // point where this code is dominated by type names?
+
+    // do not change this with using namespace. that thing is dangerous.
+
+    std::chrono::system_clock::time_point xTime = std::chrono::high_resolution_clock::now();
+
+    const double dNanoseconds = static_cast< double >( std::chrono::duration_cast< std::chrono::nanoseconds >( xTime ).count( ) );
+
+    return dNanoseconds / ( 1000.0 * 1000.0 * 1000.0 );
+}
---
+++ Time/ProfileTimer.h
@@ -0,0 +1,8 @@
+#ifndef PROFILE_TIMER_H
+#define PROFILE_TIMER_H
+
+// SE - a simple profiling timer that returns some double precision value which increases in seconds
+
+double GetProfileTimer();
+
+#endif
