d0f6d8680346c1a3a4b4a5d52da03ad827d48ee2
Semi Essessi <semi@cranium-software.co.uk>
2015-03-24 19:35:50 +0000

    placeholder for RA/Dec to ICRF vector conversion (needs doing...)

---
+++ Maths/Astrometry.cpp
@@ -0,0 +1,13 @@
+#include "Astrometry.h"
+
+namespace PDE
+{
+
+// assumes J2000.0
+Vector3 RADecToDirection( const double dRA, const double dDeclination )
+{
+	// SE - TODO: ...
+	return Vector3::Zero;
+}
+
+}
