664ac131b98ae679d0ae1020f32441f82afb46a5
Semi Essessi <semi@cranium-software.co.uk>
2015-03-24 23:45:25 +0000

    implement ra/dec to ecliptic thingy

--- Maths/Astrometry.cpp
+++ Maths/Astrometry.cpp
@@ -1,13 +1,22 @@
 #include "Astrometry.h"
 
+#include "Maths/PDEMaths.h"
+#include "Maths/PDEMatrix.h"
+#include "Maths/DPVector.h"
+
 namespace PDE
 {
 
-// assumes J2000.0
 Vector3 RADecToDirection( const double dRA, const double dDeclination )
 {
-	// SE - TODO: ...
-	return Vector3::Zero;
+    const Vector3 xEquatorial(
+        static_cast< float >( PDE::Cos( dRA ) * PDE::Cos( dDeclination ) ),
+        static_cast< float >( PDE::Sin( dRA ) * PDE::Cos( dDeclination ) ),
+        static_cast< float >( PDE::Sin( dDeclination ) ) );
+
+    // rotate it by the obliquity of the ecliptic at J2000.0...
+    static const float kfObliquity = PDE::Deg2Rad( 23.4392794f ); // 4444444444....
+    return Matrix4::RotateAxisAngle( Vector3::Left, xEquatorial, kfObliquity );
 }
 
 }
